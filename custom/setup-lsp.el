8001871b4b153298a8876c9910f7aecae479fc99
nj xiong <nj0@h0mac.lan>
2018-05-29 10:28:55 +0800

    0

--- custom/setup-lsp.el
+++ custom/setup-lsp.el
@@ -15,20 +15,35 @@
       (condition-case nil
           (lsp-cquery-enable)
         (user-error nil)))
+    (with-eval-after-load 'cquery
+      ;;(setq cquery-sem-highlight-method 'font-lock)
+      ;;(cquery-use-default-rainbow-sem-highlight)
+      (setq cquery-executable "/usr/local/bin/cquery"))
     (add-hook 'c-mode-common-hook #'cquery//enable)
-    (setq cquery-executable "/usr/local/bin/cquery")))
+    ))
 
 (use-package lsp-ui
   :init
   (progn
     (add-hook 'lsp-mode-hook 'lsp-ui-mode)
-    ))
+    (with-eval-after-load 'lsp-ui
+      ;;(define-key lsp-ui-mode-map (kbd "C-p") 'lsp-ui-peek-jump-forward)
+      ;;(define-key lsp-ui-mode-map (kbd "C-t") 'lsp-ui-peek-jump-backward)
+      (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+      (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+      (define-key lsp-ui-mode-map [remap xref-find-apropos]  #'lsp-ui-peek-find-workspace-symbol)
+      (define-key lsp-ui-mode-map (kbd "C-j") 'lsp-ui-peek-find-workspace-symbol)
+      ;(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-parse-file)
+      ;(define-key helm-gtags-mode-map (kbd "C-g") 'helm-gtags-select)
+      )))
 
 (use-package company-lsp
   :init
   (progn
-    (push 'company-lsp company-backends)
-    (setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)))
+    (with-eval-after-load 'company
+      (add-to-list 'company-backends 'company-lsp)
+      (setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)
+      )))
 
 ;; do lsp-cquery-enable on finding .cquery files in project root directory
 ;;(defun my//enable-cquery-if-compile-commands-json ()
