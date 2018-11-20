a97b6c0839452bb4ca9cd3bf4e90553d79fecbe9
noj <noj@ho.mac>
2018-11-20 22:45:01 +0800

    go

--- custom/setup-go.el
+++ custom/setup-go.el
@@ -1,3 +1,11 @@
+;; put exports below into .bash_profile
+;; export GOPATH=$HOME/Sandbox/gohome
+;; export PATH=$PATH:$GOPATH/bin:/usr/local/opt/go/libexec/bin
+;;
+;; install gocode for company-go #go get -u github.com/mdempsky/gocode (-u flag for "update")
+;; install godef fo go-mode's godef-jump  #go get -u github.com/rogpeppe/godef
+;; put company-go.el into emacs's load-path
+
 (add-to-list 'load-path "~/Sandbox/gohome/go-mode.el")
 (autoload 'go-mode "go-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
